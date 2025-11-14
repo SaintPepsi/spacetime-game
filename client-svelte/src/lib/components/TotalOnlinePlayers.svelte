3ed9e2f7ad23464d592318da9425d6744a6cf8c1
Ian Hogers <ianhogers@hotmail.com>
2025-11-14 14:55:12 +1100

    use TableQuery for total online players

--- client-svelte/src/lib/components/TotalOnlinePlayers.svelte
+++ client-svelte/src/lib/components/TotalOnlinePlayers.svelte
@@ -1,80 +1,20 @@
 <script lang="ts">
+	import { eq, TableQuery, where } from '$lib/runes/SpacetimeTable.svelte';
 	import { SpacetimeDB } from '$lib/SpacetimeDB.svelte';
-	import type { EventContext, User } from '../../module_bindings';
 
-	let onlinePlayerCount = $state(0);
-
-	let subscribeApplied = $state(false);
 	const client = SpacetimeDB.getContext();
-	const query = `SELECT * FROM user`;
-
-	let users = $state<{
-		rows: User[];
-		state: string;
-	}>({
-		rows: [],
-		state: 'loading'
-	});
 
 	let status = SpacetimeDB.status;
+	const userData = new TableQuery('user', where(eq('online', true)));
 
-	const computeSnapshot = $derived(() => {
-		const table = client.db['user'];
-		const result = table.iter();
-
-		return {
-			rows: result as unknown as User[],
-			state: subscribeApplied ? 'ready' : 'loading'
-		};
-	});
-
-	$effect(() => {
-		if ($status !== 'connected') return;
-		if (!client.isActive) return;
-		const cancel = client
-			.subscriptionBuilder()
-			.onApplied(() => {
-				subscribeApplied = true;
-				console.log('subscriptionApplied');
-				//   setSubscribeApplied(true);
-			})
-			.subscribe(query);
-		return () => {
-			cancel.unsubscribe();
-		};
-	});
-
-	$effect(() => {
-		const table = client.db['user'];
-
-		const onInsert = (ctx: EventContext, row: User) => {
-			users = computeSnapshot();
-		};
-
-		const onDelete = (ctx: EventContext, row: User) => {
-			users = computeSnapshot();
-		};
-
-		const onUpdate = (ctx: EventContext, oldRow: User, row: User) => {
-			users = computeSnapshot();
-		};
-
-		table.onInsert(onInsert);
-		table.onDelete(onDelete);
-		table.onUpdate?.(onUpdate);
-		return () => {
-			table.removeOnInsert(onInsert);
-			table.removeOnDelete(onDelete);
-			table.removeOnUpdate?.(onUpdate);
-		};
-	});
+	let onlinePlayerCount = $derived(userData.rows.length);
 </script>
 
 <div class="TotalOnlinePlayers">
 	IsActive: {client.isActive} | Status: {$status} | Online players: {onlinePlayerCount}
 </div>
 
-{#each users.rows as user (user.identity.__identity__)}
+{#each userData.rows as user (user.identity.toHexString())}
 	<div class="player">
 		{user.identity} - {user.name} - online: {user.online}
 	</div>
